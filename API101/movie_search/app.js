ac849c6d7a9fc36677879850c999d522e354e18d
syedsharjil <syedsharjil@outlook.com>
2019-02-09 00:05:13 +0530

    Basic routes added

--- API101/movie_search/app.js
+++ API101/movie_search/app.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var app = express();
 var request = require("request");
-app.get("\search",function(req,res){
-
-})
+app.get("/search",function(req,res){
+    request("http://www.omdbapi.com/?i=tt3896198&apikey=c1cb8b0c&s=California",function(error,response,body){
+        if(!error&&response.statusCode==200)
+        {
+            res.send(body);
+        }
+    })
+res.send("this is the search page");
+});
 app.listen(process.env.PORT,process.env.IP,function()
 {
     console.log("now serving your page");
