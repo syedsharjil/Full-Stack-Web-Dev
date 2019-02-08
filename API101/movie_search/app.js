9a9344958818ea77a10f79ed76999aae28f2e919
syedsharjil <syedsharjil@outlook.com>
2019-02-08 23:47:54 +0530

    Inital files added

---
+++ API101/movie_search/app.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+app.get("\search",function(req,res){
+
+})
+app.listen(process.env.PORT,process.env.IP,function()
+{
+    console.log("now serving your page");
+});
