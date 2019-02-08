28dfe59fdbca3144d2ff9bbb6fadaf8df28d10bb
syedsharjil <syedsharjil@outlook.com>
2019-02-09 02:17:30 +0530

    Templating engines set up

--- API101/movie_search/app.js
+++ API101/movie_search/app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+app.set("view engine","ejs")
 app.get("/search",function(req,res){
     request("http://www.omdbapi.com/?i=tt3896198&apikey=c1cb8b0c&t=frozen",function(error,response,body){
         if(!error && response.statusCode == 200)
