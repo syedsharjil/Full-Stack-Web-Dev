19f0aed35069ef73fd2ce19e2a3b41d2f953cb55
syedsharjil <syedsharjil@outlook.com>
2019-02-18 00:45:41 +0530

    New Search button added

--- API101/movie_search/app.js
+++ API101/movie_search/app.js
@@ -20,17 +20,3 @@ app.listen(process.env.PORT,process.env.IP,function()
 {
     console.log("now serving your page");
 });
-app.get("/results",function(req,res){
-    request("http://www.omdbapi.com/?i=tt3896198&apikey=c1cb8b0c&s=california",function(error,response,body){
-        if(!error && response.statusCode == 200)
-        {
-            var data=JSON.parse(body);          //converting into json object
-            res.render("results",{data:data}); //sending json object to EJS file to display the list
-             
-        }
-    });
-});
-app.listen(process.env.PORT,process.env.IP,function()
-{
-    console.log("now serving your page");
-});
