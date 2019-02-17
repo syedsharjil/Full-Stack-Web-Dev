8e77cce9377062e2ca27d12e865520128616c8e1
syedsharjil <syedsharjil@outlook.com>
2019-02-18 00:33:03 +0530

    User input support added

--- API101/movie_search/app.js
+++ API101/movie_search/app.js
@@ -2,7 +2,24 @@ var express = require("express");
 var app = express();
 var request = require("request");
 app.set("view engine","ejs");
-
+app.get("/",function(req,res){
+    res.render("search");
+    });
+app.get("/results",function(req,res){
+    var query=req.query.search_movie;
+    request("http://www.omdbapi.com/?i=tt3896198&apikey=c1cb8b0c&s="+query,function(error,response,body){
+        if(!error && response.statusCode == 200)
+        {
+            var data=JSON.parse(body);
+            res.render("results",{data:data});
+             
+        }
+    });
+});
+app.listen(process.env.PORT,process.env.IP,function()
+{
+    console.log("now serving your page");
+});
 app.get("/results",function(req,res){
     request("http://www.omdbapi.com/?i=tt3896198&apikey=c1cb8b0c&s=california",function(error,response,body){
         if(!error && response.statusCode == 200)
