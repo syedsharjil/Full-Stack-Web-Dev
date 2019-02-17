2a7a9da4c8fafe5bcbf5dc4b669ba344d8eb8bee
syedsharjil <syedsharjil@outlook.com>
2019-02-17 19:11:50 +0530

    Displaying data functionality added

--- API101/movie_search/app.js
+++ API101/movie_search/app.js
@@ -1,13 +1,15 @@
 var express = require("express");
 var app = express();
 var request = require("request");
-app.set("view engine","ejs")
-app.get("/search",function(req,res){
-    request("http://www.omdbapi.com/?i=tt3896198&apikey=c1cb8b0c&t=frozen",function(error,response,body){
+app.set("view engine","ejs");
+
+app.get("/results",function(req,res){
+    request("http://www.omdbapi.com/?i=tt3896198&apikey=c1cb8b0c&s=california",function(error,response,body){
         if(!error && response.statusCode == 200)
         {
-            var results=JSON.parse(body);
-            res.send(results);
+            var data=JSON.parse(body);          //converting into json object
+            res.render("results",{data:data}); //sending json object to EJS file to display the list
+             
         }
     });
 });
